692d44743ea95f148c2696d23a9bb84b2be34e02
Hannah Lee <hannahl1409@gmail.com>
2019-09-23 11:14:23 -0400

    confusion

--- manage.py
+++ manage.py
@@ -1,4 +1,6 @@
 import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 #from flask.ext.script import Manager
 #from flask.ext.migrate import Migrate, MigrateCommand
 
