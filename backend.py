959a1871702b1da1278170bdb025c6c95a5ca8b6
Hannah Lee <hannahl1409@gmail.com>
2019-09-23 10:54:13 -0400

    Database setup

--- backend.py
+++ backend.py
@@ -1,18 +1,18 @@
 #from flaskext.mysql import MySQL
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mfcxbbqeapznpi:af5713c3d90c89bf039f143c68e31d8ed47509cfbeff36157b27fa00cf4539c3@ec2-54-221-244-70.compute-1.amazonaws.com:5432/df0snl0k3sb9mq'
 
-#mysql = MySQL()
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-#Set up mySQL connection
-#app.config['MYSQL_DATABASE_USER'] = 'hts478'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'Capstonehts478'
-#app.config['MYSQL_DATABASE_DB'] = 'smartpark'
-#app.config['MYSQL_DATABASE_HOST'] = '128.180.110.111'
-#mysql.init_app(app)
+class Spot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    taken = db.Column(db.Integer)
 
-#conn = mysql.connect()
-
+    
 # root
 @app.route("/")
 def index():
