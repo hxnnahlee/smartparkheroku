11faa29c47607af9f03f9fd25e8d798870b7c5fd
Hannah Lee <hannahl1409@gmail.com>
2019-09-29 23:50:50 -0400

    why

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = "postgres://zzltjisgrkwzzl:e9d9628026cd8bd1b9a563a8d8c634a699d985dd78086bef6c0aaaa61120e5b8@ec2-54-243-253-181.compute-1.amazonaws.com:5432/djvc2b4jvr2pi"
 
 class ProductionConfig(Config):
     DEBUG = False
